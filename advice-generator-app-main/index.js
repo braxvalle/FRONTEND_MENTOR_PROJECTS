d5cb7d8248f10b4638a025a428c5d2f9c973fd4c
braxvalle <braxvalle2@gmail.com>
2024-03-05 21:47:40 +0300

    fetc fixing

--- advice-generator-app-main/index.js
+++ advice-generator-app-main/index.js
@@ -1,13 +1,17 @@
-const id=document.getElementById("id");
-const advice=document.getElementById("advice");
-const dice=document.querySelector(".dice");
+const idElement = document.getElementById("id-no");
+const adviceElement = document.getElementById("advice");
+const dice = document.getElementById("dice");
 
-dice.addEventListener('click', () => {
-    fetch('https://api.adviceslip.com')
-        .then(res => res.json())
-        .then(data => {
-            id.innerHTML = data.slip.id;
-            advice.textContent = data.slip.advice
-        })
-})
-
+dice.addEventListener("click", () => {
+  fetch("https://api.adviceslip.com/advice")
+    .then((res) => res.json())
+    .then((data) => {
+      if (idElement && adviceElement) {
+        idElement.innerHTML = data.slip.id;
+        adviceElement.textContent = data.slip.advice;
+      }
+    })
+    .catch((error) => {
+      console.log("error fetching data");
+    });
+});
