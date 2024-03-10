12ec7d0be5d69b73508266b3f315e6643f72cf5c
braxvalle <braxvalle2@gmail.com>
2024-03-10 14:34:46 +0300

    erro handling

--- interactive-card-details-form-main/index.js
+++ interactive-card-details-form-main/index.js
@@ -1,9 +1,33 @@
-const form = document.getElementById('form');
-const name = document.getElementById('name');
+const form = document.getElementById("form");
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    if(name === '') {
-        console.log('Please fill all the required fields')
-    }
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (validateForm()) {
+    alert("Successfully submitted!");
+  }
+});
+
+function validateForm() {
+  let isValid = true;
+
+  const name = document.getElementById("name");
+  const errorName = document.getElementById("error-name");
+  if (name.value === "") {
+    errorName.textContent = "Please enter your name";
+    isValid = false;
+  } else {
+    errorName.textContent = "";
+  }
+
+  const cardNo = document.getElementById("card-Number");
+  const errorNo = document.getElementById("error-no");
+  if (cardNo.value === "") {
+    errorNo.textContent = "Wrong format numbers only";
+    isValid = false;
+  } else {
+    errorNo.textContent = "";
+  }
+
+  return isValid;
+}
