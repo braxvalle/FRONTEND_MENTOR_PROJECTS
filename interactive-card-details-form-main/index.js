9aaef34037fa1515868825bf9261c0a893ece940
braxvalle <braxvalle2@gmail.com>
2024-03-11 15:12:34 +0300

    Changes

--- interactive-card-details-form-main/index.js
+++ interactive-card-details-form-main/index.js
@@ -1,9 +1,18 @@
-const form = document.getElementById('form');
-const name = document.getElementById('name');
+// const form = document.getElementById('form');
+// const name = document.getElementById('name');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    if(name === '') {
-        console.log('Please fill all the required fields')
-    }
-})
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault()
+//     if(name === '') {
+//         console.log('Please fill all the required fields')
+//     }
+// })
+
+const foo  = (n) => {
+    if(n <= 2) return 1;
+    return foo(n - 1) + foo(n - 2);
+}
+
+console.log(foo(9));
+console.log(foo(1));
+console.log(foo(5));
