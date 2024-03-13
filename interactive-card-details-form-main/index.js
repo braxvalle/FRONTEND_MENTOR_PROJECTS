99e4e19359fec045608d3a42ddb365121e9bf528
braxvalle <braxvalle2@gmail.com>
2024-03-13 21:28:27 +0300

    local changes

--- interactive-card-details-form-main/index.js
+++ interactive-card-details-form-main/index.js
@@ -3,11 +3,20 @@ const container = document.getElementById("form-container");
 const thanks = document.getElementById("thanks-container");
 const name = document.getElementById("name");
 const errorName = document.getElementById("error-name");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+const cardNo = document.getElementById("card-Number");
+const cvc = document.getElementById("cvc");
 
+const cardDigits = document.getElementById("card_no");
 const displayName = document.getElementById("display-name");
 
 name.addEventListener("input", () => {
   displayName.textContent = name.value;
+});
+
+cardNo.addEventListener("input", () => {
+  cardDigits.textContent = cardNo.value;
 });
 
 form.addEventListener("submit", (e) => {
@@ -29,7 +38,6 @@ function validateForm() {
     errorName.textContent = "";
   }
 
-  const cardNo = document.getElementById("card-Number");
   const errorNo = document.getElementById("error-no");
   if (cardNo.value === "") {
     errorNo.textContent = "Please enter your card number";
@@ -41,13 +49,20 @@ function validateForm() {
     errorNo.textContent = "";
   }
 
-  const cvc = document.getElementById("cvc");
   const errorCVC = document.getElementById("error-cvc");
   if (cvc.value === "") {
     errorCVC.textContent = "Can't be black";
     isValid = false;
   } else {
     errorCVC.textContent = "";
+  }
+
+  const errorDate = document.getElementById("error-date");
+  if (month.value === "" || year.value === "") {
+    errorDate.textContent = "Can't be blank";
+    isValid = false;
+  } else {
+    errorDate.textContent = "";
   }
   return isValid;
 }
