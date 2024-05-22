21f4f52828b14c33b15951e2920c022984f25315
braxvalle <braxvalle2@gmail.com>
2024-05-22 20:59:07 +0300

    updates

---
+++ multi-step-form-main/index.js
@@ -0,0 +1,8 @@
+const next = document.querySelectorAll("#next");
+const back = document.querySelectorAll("#back");
+
+next.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    alert("clicked me niqqa");
+  });
+});
