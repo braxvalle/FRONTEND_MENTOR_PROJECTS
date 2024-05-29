8ec018348b60a579ab6a06ab3393a42d4e1e07ba
braxvalle <braxvalle2@gmail.com>
2024-05-29 05:58:38 +0300

    updating javascript functions

--- multi-step-form-main/index.js
+++ multi-step-form-main/index.js
@@ -1,8 +1,29 @@
-const next = document.querySelectorAll("#next");
-const back = document.querySelectorAll("#back");
+const next = document.getElementById("nextBtn");
+const back = document.getElementById("backBtn");
+const steps = document.querySelectorAll('.step');
+let currentStep = 1;
 
-next.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    alert("clicked me niqqa");
+function updateStep(step) {
+  steps.forEach((s, index) => {
+    if (index + 1 === step) {
+      s.classList.add('active');
+    } else {
+      s.classList.remove('active');
+    }
   });
-});
+  currentStep = step;
+}
+
+
+next.addEventListener('click', () => {
+  if(currentStep < steps.length) {
+    updateStep(currentStep + 1)
+    return
+  }
+})
+
+back.addEventListener('click', () => {
+  if (currentStep > 1) {
+    updateStep(currentStep - 1)
+  }
+})
