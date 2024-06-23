25fa01806ee80e93b834759a4de3c972683ceee1
braxvalle <braxvalle2@gmail.com>
2024-06-23 17:34:18 +0300

    pulls

--- multi-step-form-main/index.js
+++ multi-step-form-main/index.js
@@ -6,102 +6,89 @@ const steps = document.querySelectorAll(".step");
 let currentStep = 0;
 
 //form section
-next.addEventListener("click", (e) => {
-  const form = document.querySelector("#form");
+next.addEventListener('click', (e) => {
+  const form = document.querySelector('#form');
 
-  if (currentStep < steps.length && validateForm()) {
-    updateStep(currentStep + 1);
-    e.preventDefault();
+  if(currentStep < steps.length && validateForm()) {
+    updateStep(currentStep + 1)
+    e.preventDefault()
   } else {
-    e.preventDefault();
-    console.log("Form is valid");
+    e.preventDefault()
+    console.log('Form is valid')
   }
 
   function validateForm() {
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const phone = document.getElementById("phone").value;
-
-    document.getElementById("error-name").innerHTML = "";
-    document.getElementById("error-email").innerHTML = "";
-    document.getElementById("error-phone").innerHTML = "";
-
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+  
+    document.getElementById('error-name').innerHTML = "";
+    document.getElementById('error-email').innerHTML = "";
+    document.getElementById('error-phone').innerHTML = "";
+  
     let isValid = true;
-
-    if (name === "") {
-      document.getElementById("error-name").textContent =
-        "This field is required";
+  
+    if(name === '') {
+      document.getElementById('error-name').textContent = "This field is required";
       isValid = false;
     }
-    if (email === "") {
-      document.getElementById("error-email").textContent =
-        "This field is required";
+    if(email === '') {
+      document.getElementById('error-email').textContent = "This field is required";
       isValid = false;
     }
-    if (phone === "") {
-      document.getElementById("error-phone").textContent =
-        "This field is required";
+    if(phone === '') {
+      document.getElementById('error-phone').textContent = "This field is required";
       isValid = false;
     }
     return isValid;
   }
-});
+})
+
 
 function updateStep(step) {
-  steps.forEach((S, index) => {
+  steps.forEach((s, index) => {
     if (index === step) {
-      S.classList.add("active");
+      s.classList.add('active');
     } else {
-      S.classList.remove("active");
+      s.classList.remove('active');
     }
   });
 
   currentStep = step;
 
   nextBtns.forEach((nextBtn) => {
-    nextBtn.addEventListener("click", () => {
-      if (currentStep < 4) {
-        updateStep(currentStep + 1);
+    nextBtn.addEventListener('click', () => {
+      if(currentStep < 4) {
+        updateStep(currentStep + 1)
       }
-      console.log("Clicked");
-    });
-  });
+      console.log('Clicked')
+    })
+    
+  })
 
   backs.forEach((back) => {
-    back.addEventListener("click", () => {
+    back.addEventListener('click', () => {
       if (currentStep > 0) {
-        updateStep(currentStep - 1);
+        updateStep(currentStep - 1)
       }
-      console.log("Clicked");
-    });
-  });
+      console.log('Clicked')
+    })
+  })
 }
 
-next.addEventListener("click", () => {
-  if (currentStep < steps.length) {
-    updateStep(currentStep + 1);
-  }
-});
-
-back.addEventListener("click", () => {
-  if (currentStep > 0) {
-    updateStep(currentStep - 1);
-  }
-});
-
 // Toggle button functions:
-const slider = document.querySelector(".slider");
-const months = document.querySelector(".swi-month");
-const years = document.querySelector(".swi-year");
+const slider = document.querySelector('.slider');
+const months = document.querySelector('.swi-month');
+const years = document.querySelector('.swi-year');
 
-slider.addEventListener("click", () => {
-  slider.classList.toggle("clicked");
+slider.addEventListener('click', () => {
+  slider.classList.toggle('clicked')
 
-  if (slider.classList.contains("clicked")) {
-    months.classList.add("active");
-    years.classList.remove("active");
+  if(slider.classList.contains('clicked')) {
+    months.classList.add('active')
+    years.classList.remove('active')
   } else {
-    months.classList.remove("active");
-    years.classList.add("active");
+    months.classList.remove('active')
+    years.classList.add('active')
   }
 });
