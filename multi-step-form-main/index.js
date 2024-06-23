ac5209b82ed7b2ce6810123e9f23bd5363464f31
braxvalle <braxvalle2@gmail.com>
2024-06-23 10:53:51 +0300

    form updates

--- multi-step-form-main/index.js
+++ multi-step-form-main/index.js
@@ -1,7 +1,49 @@
-const next = document.getElementById("nextBtn");
-const back = document.getElementById("backBtn");
+const next = document.querySelector("#nextBtn");
+const nextBtns = document.querySelectorAll(".nextBtn");
+const backs = document.querySelectorAll(".backBtn");
 const steps = document.querySelectorAll(".step");
+
 let currentStep = 0;
+
+//form section
+next.addEventListener('click', (e) => {
+  const form = document.querySelector('#form');
+
+  if(currentStep < steps.length && validateForm()) {
+    updateStep(currentStep + 1)
+    e.preventDefault()
+  } else {
+    e.preventDefault()
+    console.log('Form is valid')
+  }
+
+  function validateForm() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+  
+    document.getElementById('error-name').innerHTML = "";
+    document.getElementById('error-email').innerHTML = "";
+    document.getElementById('error-phone').innerHTML = "";
+  
+    let isValid = true;
+  
+    if(name === '') {
+      document.getElementById('error-name').textContent = "This field is required";
+      isValid = false;
+    }
+    if(email === '') {
+      document.getElementById('error-email').textContent = "This field is required";
+      isValid = false;
+    }
+    if(phone === '') {
+      document.getElementById('error-phone').textContent = "This field is required";
+      isValid = false;
+    }
+    return isValid;
+  }
+})
+
 
 function updateStep(step) {
   steps.forEach((s, index) => {
@@ -11,21 +53,28 @@ function updateStep(step) {
       s.classList.remove('active');
     }
   });
+
   currentStep = step;
+
+  nextBtns.forEach((nextBtn) => {
+    nextBtn.addEventListener('click', () => {
+      if(currentStep < 4) {
+        updateStep(currentStep + 1)
+      }
+      console.log('Clicked')
+    })
+    
+  })
+
+  backs.forEach((back) => {
+    back.addEventListener('click', () => {
+      if (currentStep > 0) {
+        updateStep(currentStep - 1)
+      }
+      console.log('Clicked')
+    })
+  })
 }
-
-
-next.addEventListener('click', () => {
-  if(currentStep < steps.length) {
-    updateStep(currentStep + 1)
-  }
-})
-
-back.addEventListener('click', () => {
-  if (currentStep > 0) {
-    updateStep(currentStep - 1)
-  }
-})
 
 // Toggle button functions:
 const slider = document.querySelector('.slider');
@@ -43,11 +92,3 @@ slider.addEventListener('click', () => {
     years.classList.add('active')
   }
 });
-
-//form section
-const form = document.querySelector('#form');
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault()
-  console.log('submiteed')
-})
